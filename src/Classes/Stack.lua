43f9274979a184ea577f2fdcbaee1cb94075ce43
Rythian Smythe <rythian@rythian2277.dev>
2021-05-04 01:51:20 +0100

    Create Stack.lua

---
+++ src/Classes/Stack.lua
@@ -0,0 +1,25 @@
+local Class = require(3696101309)
+local Stack = Class() do
+	function Stack:init()
+		self._Storage = {}
+		return self
+	end
+	
+	function Stack:Pop()
+		table.remove(self._Storage, #self._Storage)
+	end
+	
+	function Stack:Peek()
+		return self._Storage[#self._Storage]
+	end
+	
+	function Stack:Push(data)
+		table.insert(self._Storage, data)
+	end
+end
+		
+return setmetatable({ new = Stack }, {
+    __call = function (_, ...)
+        return Stack(...)
+    end
+})
